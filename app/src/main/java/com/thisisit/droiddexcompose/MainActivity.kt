fbdad27126c0ad89bd483932d2544fedc50e2184
Rodney <123950191+rodneyd05@users.noreply.github.com>
2023-11-26 22:36:09 +0800

    Initial Commit

---
+++ app/src/main/java/com/thisisit/droiddexcompose/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.thisisit.droiddexcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.thisisit.droiddexcompose.ui.theme.DroidDexComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DroidDexComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("DroidDex")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = name,
+        modifier = modifier
+    )
+}
